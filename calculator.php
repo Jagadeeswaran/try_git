bbce08b831f181e6b997ecdf771aaa5705f0fb31
Jagadeeswaran <jagadeeswaran@enqos.com>
2012-11-07 18:32:37 +0530

    test commit

---
+++ calculator.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of calculator
+ *
+ * @author Jagadees
+ */
+class calculator {
+    //put your code here
+    /**
+     * @assert (0, 0) == 0
+     * @assert (0, 1) == 1
+     * @assert (1, 0) == 1
+     * @assert (1, 1) == 2     
+     * @assert (1, 2) == 3
+     */
+    public function add($a, $b) {
+        return $a+$b;
+    }
+    
+    /**
+     * @assert (0, 0) == 0
+     * @assert (2, 1) == 1
+     * @assert (1, 2) == -1
+     */
+    public function sub($a,$b) {
+        return $a-$b;
+    }
+}
+
+?>
